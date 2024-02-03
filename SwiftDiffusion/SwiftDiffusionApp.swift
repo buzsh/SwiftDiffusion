673f5f1c40251a03a701eeb79b246c36e4b06523
Justin Bush <jdevbush@gmail.com>
2024-02-03 14:26:27 -0700

    feat: terminateScript on appWillTerminate

--- SwiftDiffusion/SwiftDiffusionApp.swift
+++ SwiftDiffusion/SwiftDiffusionApp.swift
@@ -5,13 +5,24 @@
 //  Created by Justin Bush on 2/3/24.
 //
 
+import Cocoa
 import SwiftUI
 
 @main
 struct SwiftDiffusionApp: App {
+  @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+  
   var body: some Scene {
     WindowGroup {
       ContentView()
     }
+  }
+}
+
+
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+  func applicationWillTerminate(_ notification: Notification) {
+    ScriptManager.shared.terminateScript()
   }
 }
