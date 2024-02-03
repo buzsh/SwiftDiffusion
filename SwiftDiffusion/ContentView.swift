c68cb0f6362b5ec68051b894fb86d37206e5a742
Justin Bush <jdevbush@gmail.com>
2024-02-03 13:15:00 -0700

    feat: SwiftUI project

---
+++ SwiftDiffusion/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftDiffusion
+//
+//  Created by Justin Bush on 2/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  var body: some View {
+    VStack {
+      Image(systemName: "globe")
+        .imageScale(.large)
+        .foregroundStyle(.tint)
+      Text("Hello, world!")
+    }
+    .padding()
+  }
+}
+
+#Preview {
+  ContentView()
+}
---
+++ SwiftDiffusion/SwiftDiffusionApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDiffusionApp.swift
+//  SwiftDiffusion
+//
+//  Created by Justin Bush on 2/3/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDiffusionApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
