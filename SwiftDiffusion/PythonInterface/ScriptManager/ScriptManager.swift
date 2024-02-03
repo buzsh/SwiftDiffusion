18cc003edc6d1388509860803c4b72ce1470bc2d
Justin Bush <jdevbush@gmail.com>
2024-02-03 16:55:44 -0700

    refactor: ScriptManager, State, availability

--- SwiftDiffusion/PythonInterface/ScriptManager/ScriptManager.swift
+++ SwiftDiffusion/PythonInterface/ScriptManager/ScriptManager.swift
@@ -12,22 +12,57 @@ enum ScriptResult {
   case failure(Error)
 }
 
+enum ScriptState {
+  case readyToStart
+  case launching
+  case active(String)
+  case isTerminating
+  case terminated
+}
+
+extension ScriptManager {
+  var scriptStateText: String {
+    switch scriptState {
+    case .readyToStart:
+      return "Ready to start"
+    case .launching:
+      return "Launching service..."
+    case .active:
+      if let urlString = self.parsedURL?.absoluteString {
+        return "Active (\(urlString))"
+      } else {
+        return "Active"
+      }
+    case .isTerminating:
+      return "Terminating..."
+    case .terminated:
+      return "Terminated"
+    }
+  }
+}
+
 class ScriptManager: ObservableObject {
   static let shared = ScriptManager()
   private var process: Process?
   private var outputPipe: Pipe?
   private var errorPipe: Pipe?
   
+  private(set) var serviceURL: URL?
+  @Published var parsedURL: URL? = nil
+  
   private let configManager: ConfigFileManager?
   private var originalLaunchBrowserLine: String?
   
+  var shouldTrimOutput: Bool = false
+  
+  @Published var scriptState: ScriptState = .readyToStart
   @Published var consoleOutput: String = ""
   var scriptPath: String? {
     didSet {
       UserDefaults.standard.set(scriptPath, forKey: "scriptPath")
     }
   }
-  
+  /// Initializes a new instance of `ScriptManager`.
   init() {
     self.scriptPath = UserDefaults.standard.string(forKey: "scriptPath")
     if let scriptPath = self.scriptPath {
@@ -36,72 +71,96 @@ class ScriptManager: ObservableObject {
       self.configManager = nil
     }
   }
-  
+  /// Updates the console output with a new message.
+  /// - Parameter message: The message to be added to the console output.
   private func updateConsoleOutput(with message: String) {
     DispatchQueue.main.async {
       self.consoleOutput += "\n\(message)"
     }
   }
-  
+  /// Starts the execution of the Automatic1111 Python script.
   func runScript() {
+    self.scriptState = .launching
+    print("Script starting...") // Debugging log
+    // Reset the service URL each time the script runs to ensure fresh capture
+    self.serviceURL = nil
+    
     guard let scriptPath = scriptPath, !scriptPath.isEmpty else { return }
     let scriptDirectory = URL(fileURLWithPath: scriptPath).deletingLastPathComponent().path
     let scriptName = URL(fileURLWithPath: scriptPath).lastPathComponent
     
     process = Process()
-    let pipe = Pipe()
+    let outputPipe = Pipe()
+    let errorPipe = Pipe()
+    self.outputPipe = outputPipe
+    self.errorPipe = errorPipe
     
     disableLaunchBrowserInConfigJson()  // not detrimental if fails, but stops webui from launching in browser on startup
     
     process?.executableURL = URL(fileURLWithPath: "/bin/zsh")
-    process?.arguments = ["-c", "cd \(scriptDirectory); ./\(scriptName) --autolaunch"]
-    process?.standardOutput = pipe
-    process?.standardError = pipe
+    process?.arguments = ["-c", "cd \(scriptDirectory); ./\(scriptName)"]
+    process?.standardOutput = outputPipe
+    process?.standardError = errorPipe
     
-    pipe.fileHandleForReading.readabilityHandler = { [weak self] fileHandle in
-      if let output = String(data: fileHandle.availableData, encoding: .utf8) {
-        DispatchQueue.main.async {
-          self?.updateConsoleOutput(with: output)
+    let outputHandler: (FileHandle) -> Void = { [weak self] fileHandle in
+      let data = fileHandle.availableData
+      guard !data.isEmpty else { return }
+      let output = String(data: data, encoding: .utf8) ?? ""
+      
+      // Use the flag to determine if output should be trimmed
+      let finalOutput = self?.shouldTrimOutput == true ? output.trimmingCharacters(in: .whitespacesAndNewlines) : output
+      
+      DispatchQueue.main.async {
+        self?.consoleOutput += "\n\(finalOutput)"
+        // Parse for URL if not already found
+        if self?.serviceURL == nil, finalOutput.contains("Running on local URL") {
+          self?.parseForServiceURL(from: finalOutput)
         }
       }
     }
+    
+    // Assign the handler to both output and error pipes
+    outputPipe.fileHandleForReading.readabilityHandler = outputHandler
+    errorPipe.fileHandleForReading.readabilityHandler = outputHandler
     
     do {
       try process?.run()
     } catch {
-      updateConsoleOutput(with: "Failed to start script: \(error.localizedDescription)")
+      DispatchQueue.main.async {
+        self.updateConsoleOutput(with: "Failed to start script: \(error.localizedDescription)")
+      }
     }
   }
   
   func terminateScript(completion: @escaping (ScriptResult) -> Void) {
-    guard let process = process else {
-      completion(.failure(NSError(domain: "ScriptManagerError", code: 0, userInfo: [NSLocalizedDescriptionKey: "Process not running."])))
-      return
-    }
+    self.scriptState = .isTerminating
     
-    // Set the termination handler
-    process.terminationHandler = { [weak self] process in
-      // Ensure we capture any final output
-      let outputData = self?.outputPipe?.fileHandleForReading.availableData
-      if let output = String(data: outputData ?? Data(), encoding: .utf8), !output.isEmpty {
+    DispatchQueue.global(qos: .background).async {
+      self.restoreLaunchBrowserInConfigJson()
+      
+      guard let process = self.process else {
         DispatchQueue.main.async {
-          self?.updateConsoleOutput(with: output)
+          completion(.failure(NSError(domain: "ScriptManagerError", code: 0, userInfo: [NSLocalizedDescriptionKey: "No process is currently running."])))
         }
+        return
       }
       
-      DispatchQueue.main.async {
-        if process.terminationStatus == 0 {
-          completion(.success("Script terminated successfully."))
-        } else {
-          completion(.failure(NSError(domain: "ScriptManagerError", code: Int(process.terminationStatus), userInfo: [NSLocalizedDescriptionKey: "Script terminated with errors."])))
+      process.terminate()
+      self.clearPipeHandlers()
+      
+      DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+        self.checkScriptServiceAvailability { isRunning in
+          if !isRunning {
+            completion(.success("Script terminated successfully and the service is no longer accessible."))
+            self.scriptState = .terminated
+            self.parsedURL = nil
+            self.updateConsoleOutput(with: "Service terminated.")
+          } else {
+            completion(.failure(NSError(domain: "ScriptManagerError", code: 1, userInfo: [NSLocalizedDescriptionKey: "The script's web service is still accessible after attempting to terminate the script."])))
+          }
         }
-        // Clear handlers and pipes after capturing final output
-        self?.clearPipeHandlers()
       }
     }
-    
-    // Terminate the process
-    process.terminate()
   }
   
   
@@ -146,4 +205,52 @@ class ScriptManager: ObservableObject {
     }
   }
   
+  private func parseForServiceURL(from output: String) {
+    let pattern = "Running on local URL: \\s*(http://127\\.0\\.0\\.1:\\d+)"
+    do {
+      let regex = try NSRegularExpression(pattern: pattern)
+      let nsRange = NSRange(output.startIndex..<output.endIndex, in: output)
+      if let match = regex.firstMatch(in: output, options: [], range: nsRange) {
+        let urlRange = Range(match.range(at: 1), in: output)!
+        let url = String(output[urlRange])
+        self.parsedURL = URL(string: url)
+        DispatchQueue.main.async {
+          self.scriptState = .active(url)
+          print("URL successfully parsed and state updated to active: \(url)")
+        }
+      } else {
+        print("No URL match found.")
+      }
+    } catch {
+      print("Regex error: \(error)")
+    }
+  }
+  
+  
+}
+
+
+extension ScriptManager {
+  
+  func checkScriptServiceAvailability(completion: @escaping (Bool) -> Void) {
+    guard let url = serviceURL else {
+      print("Service URL not available.")
+      completion(false)
+      return
+    }
+    
+    let task = URLSession.shared.dataTask(with: url) { _, response, error in
+      DispatchQueue.main.async {
+        if let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 {
+          // The page loaded successfully, script is likely still running
+          completion(true)
+        } else {
+          // The request failed, script is likely terminated
+          completion(false)
+        }
+      }
+    }
+    
+    task.resume()
+  }
 }
