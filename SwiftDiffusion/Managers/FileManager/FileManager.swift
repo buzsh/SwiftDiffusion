e579d5bfbf4462205c5ca5546292ab3832a3eb73
Justin Bush <jdevbush@gmail.com>
2024-02-05 10:31:36 -0700

    feat: Constants, FileManager, ConsoleView

---
+++ SwiftDiffusion/Managers/FileManager/FileManager.swift
@@ -0,0 +1,9 @@
+//
+//  FileManager.swift
+//  SwiftDiffusion
+//
+//  Created by Justin Bush on 2/5/24.
+//
+
+import Foundation
+
---
+++ SwiftDiffusion/Views/ConsoleView.swift
@@ -0,0 +1,18 @@
+//
+//  ConsoleView.swift
+//  SwiftDiffusion
+//
+//  Created by Justin Bush on 2/5/24.
+//
+
+import SwiftUI
+
+struct ConsoleView: View {
+  var body: some View {
+    Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  }
+}
+
+#Preview {
+  ConsoleView()
+}
