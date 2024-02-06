efd2cea57bacd155f33f4b8383c8ef0e6522f328
Justin Bush <jdevbush@gmail.com>
2024-02-06 09:22:27 -0700

    refactor: ThumbnailLoader @MainActor

--- SwiftDiffusion/Views/DetailView/ThumbnailLoader.swift
+++ SwiftDiffusion/Views/DetailView/ThumbnailLoader.swift
@@ -5,6 +5,7 @@
 //  Created by Justin Bush on 2/6/24.
 //
 
+import Cocoa
 import SwiftUI
 
 class ThumbnailLoader: ObservableObject {
@@ -12,30 +13,35 @@ class ThumbnailLoader: ObservableObject {
   @Published var imageSize: CGSize?
   @Published var fileSize: String = ""
   
-  func loadThumbnail(for node: FileNode) {
-    DispatchQueue.global(qos: .userInitiated).async {
-      //let fileURL = URL(fileURLWithPath: node.fullPath)
-      guard let image = NSImage(contentsOfFile: node.fullPath) else {
-        DispatchQueue.main.async {
-          self.thumbnailImage = NSImage(systemSymbolName: "photo.fill", accessibilityDescription: nil) ?? NSImage()
+  func loadThumbnail(for node: FileNode) async {
+    // load the image and process it on the main actor to avoid concurrency issues
+    await processImage(from: node.fullPath)
+    // fetch and format file size on a background thread and then update on the main actor
+    await fetchAndSetFileSize(for: node.fullPath)
+  }
+  
+  @MainActor
+  private func processImage(from path: String) {
+    guard let image = NSImage(contentsOfFile: path) else {
+      self.thumbnailImage = nil
+      return
+    }
+    
+    let thumbnail = image.resizedToMaintainAspectRatio(targetHeight: 40)
+    self.thumbnailImage = thumbnail
+    self.imageSize = image.size // original image size
+  }
+  
+  private func fetchAndSetFileSize(for path: String) async {
+    do {
+      let fileSizeAttributes = try FileManager.default.attributesOfItem(atPath: path)
+      if let fileSize = fileSizeAttributes[.size] as? NSNumber {
+        await MainActor.run {
+          self.fileSize = formatFileSize(fileSize.intValue)
         }
-        return
       }
-      
-      let thumbnail = image.resizedToMaintainAspectRatio(targetHeight: 40)
-      
-      // Fetch and format file size
-      let fileSizeAttributes = try? FileManager.default.attributesOfItem(atPath: node.fullPath)
-      if let fileSize = fileSizeAttributes?[.size] as? NSNumber {
-        DispatchQueue.main.async {
-          self.fileSize = self.formatFileSize(fileSize.intValue)
-        }
-      }
-      
-      DispatchQueue.main.async {
-        self.thumbnailImage = thumbnail
-        self.imageSize = image.size // original image size
-      }
+    } catch {
+      print("Error fetching file size: \(error)")
     }
   }
   
