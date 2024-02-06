f2780b48e43cb0d6ad36a89b81389c685fcf4442
Justin Bush <jdevbush@gmail.com>
2024-02-06 07:39:01 -0700

    feat: SettingsView

---
+++ SwiftDiffusion/Views/SettingsView/SettingsView.swift
@@ -0,0 +1,67 @@
+//
+//  SettingsView.swift
+//  SwiftDiffusion
+//
+//  Created by Justin Bush on 2/6/24.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+  @Binding var scriptPathInput: String
+  @Binding var fileOutputDir: String
+  
+  var body: some View {
+    VStack(alignment: .leading) {
+      
+      Text("webui.sh path")
+        .padding(.horizontal, 14)
+        .font(.system(.body, design: .monospaced))
+      
+      HStack {
+        TextField("Path to webui.sh", text: $scriptPathInput)
+          .textFieldStyle(RoundedBorderTextFieldStyle())
+          .font(.system(.body, design: .monospaced))
+        Button("Browse...") {
+          browseForWebuiShell()
+        }
+      }
+      
+      Text("image output directory")
+        .padding(.horizontal, 14)
+        .font(.system(.body, design: .monospaced))
+      
+      HStack {
+        TextField("Path to webui.sh", text: $fileOutputDir)
+          .textFieldStyle(RoundedBorderTextFieldStyle())
+          .font(.system(.body, design: .monospaced))
+        Button("Browse...") {
+          browseForOutputDirectory()
+        }
+      }
+      
+    }
+    .padding(14)
+  }
+  
+  /// Allows the user to browse for `webui.sh` and sets the associated path variables
+  func browseForWebuiShell() {
+    Task {
+      if let path = await FilePickerService.browseForShellFile() {
+        self.scriptPathInput = path
+      }
+    }
+  }
+  
+  func browseForOutputDirectory() {
+    Task {
+      if let path = await FilePickerService.browseForDirectory() {
+        self.fileOutputDir = path
+      }
+    }
+  }
+}
+
+#Preview {
+  SettingsView(scriptPathInput: .constant("path/to/webui.sh"), fileOutputDir: .constant("path/to/outputs/"))
+}
