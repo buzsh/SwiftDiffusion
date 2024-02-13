362f69dc827f3964af4b3d2039e552ac0af2e7ca
Justin Bush <jdevbush@gmail.com>
2024-02-13 12:11:11 -0700

    refactor: additional UserSettings documentation comments.

--- SwiftDiffusion/SettingsView/UserSettings.swift
+++ SwiftDiffusion/SettingsView/UserSettings.swift
@@ -13,29 +13,41 @@ class UserSettings: ObservableObject {
   static let shared = UserSettings()
   let store = UserDefaults.standard
   
+  /// Indicates whether help descriptions in the SettingsView should always be shown to the user.
+  /// Enabling this provides users with more context for each setting, potentially improving usability.
+  /// - Note: Changes to this property are automatically persisted to `UserDefaults`.
   @Published var alwaysShowSettingsHelp: Bool {
     didSet { store.set(alwaysShowSettingsHelp, forKey: "alwaysShowSettingsHelp") }
   }
   
+  
   // MARK: - File Path Settings
+  /// Path to the Automatic1111's webui.sh shell script used by the application.
+  /// This path is persisted across app launches and is used to execute Automatic1111's web UI shell script.
+  /// - Note: Changes to this property are automatically persisted to `UserDefaults`.
   @Published var webuiShellPath: String {
     didSet { store.set(webuiShellPath, forKey: "webuiShellPath") }
   }
-  
+  /// Path to the directory containing the Stable Diffusion models.
+  /// This setting is essential for the application to locate and use the stable diffusion model checkpoints.
+  /// - Note: Changes to this property are automatically persisted to `UserDefaults`.
   @Published var stableDiffusionModelsPath: String {
     didSet { store.set(stableDiffusionModelsPath, forKey: "stableDiffusionModelsPath") }
   }
-  
+  /// Custom directory path chosen by the user for saving image outputs.
+  /// Allows users to specify a preferred location for saving generated images.
+  /// - Note: Changes to this property are automatically persisted to `UserDefaults`.
   @Published var outputDirectoryPath: String {
     didSet { store.set(outputDirectoryPath, forKey: "outputDirectoryPath") }
   }
   
+  
   // MARK: - Developer Settings
   /// Indicates whether all Python processes should be terminated when the app is terminated.
-  /// - important: There is currently no external implementation for handling when the app is exited from Xcode's developer interface.
+  /// - Warning: There is currently no external implementation for handling when the app is exited from Xcode's developer interface.
   /// As a result, all Python processes launched during runtime will persist if the application is exited in this manor. Enabling this option will not resolve this issue,
   /// but it does allow you to kill all Python processes from previous app runetime states when terminated via the "Stop" square action in the toolbar.
-  /// - Note: Python processes launched within the regular application runtime–outside of the developer environment–are handled via
+  /// - Important: Python processes launched within the regular application runtime–outside of the developer environment–are handled via
   /// AppDelegate's `applicationShouldTerminate` method.
   @Published var killAllPythonProcessesOnTerminate: Bool {
     didSet { store.set(killAllPythonProcessesOnTerminate, forKey: "killAllPythonProcessesOnTerminate") }
@@ -75,16 +87,26 @@ class UserSettings: ObservableObject {
     }
   }
   
+  
   // MARK: - Prompt Settings
+  /// Controls whether automatic parsing of generation data from the clipboard is disabled.
+  /// When enabled, the app will not automatically parse and format generation data copied from external sources such as Civit.ai.
+  /// - Note: Changes to this property are automatically persisted to `UserDefaults`.
   @Published var disablePasteboardParsingForGenerationData: Bool {
     didSet { store.set(disablePasteboardParsingForGenerationData, forKey: "disablePasteboardParsingForGenerationData") }
   }
-  
+  /// Determines if the 'Paste Generation Data' button is always visible, regardless of clipboard content compatibility. Enabling this will also disablePasteboardParsingForGenerationData.
+  /// This can be useful for users who don't want their clipboards constantly being parsed for new generation data by the application.
+  /// - Note: Changes to this property are automatically persisted to `UserDefaults`.
   @Published var alwaysShowPasteboardGenerationDataButton: Bool {
     didSet { store.set(alwaysShowPasteboardGenerationDataButton, forKey: "alwaysShowPasteboardGenerationDataButton") }
   }
   
   // MARK: Engine Settings
+  /// Disables RAM optimizations during model loading.
+  /// This setting may help resolve issues related to model loading errors–such as MPS, BFloat16, BFloat32. However, it can increase load times significantly.
+  /// - Warning: Enabling this setting can significantly increase the time it takes to load models. Only enable this if the console is returning issues involving MPS, BFloat16, BFloat32.
+  /// - Note: Changes to this property are automatically persisted to `UserDefaults`.
   @Published var disableModelLoadingRamOptimizations: Bool {
     didSet { store.set(disableModelLoadingRamOptimizations, forKey: "disableModelLoadingRamOptimizations") }
   }
@@ -123,6 +145,7 @@ class UserSettings: ObservableObject {
     disableModelLoadingRamOptimizations = false
   }
 }
+
 
 extension UserSettings {
   /// Updates the visibility state of Python environment controls based on the current state of the developer interface.
