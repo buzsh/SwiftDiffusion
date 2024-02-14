73a03a0b4b585cac89aa085e21e3cfcb0a064f2d
Justin Bush <jdevbush@gmail.com>
2024-02-13 22:39:14 -0700

    refactor: working List implementation

--- SwiftDiffusion/ContentView/ContentView.swift
+++ SwiftDiffusion/ContentView/ContentView.swift
@@ -37,9 +37,6 @@ struct ContentView: View {
   @ObservedObject var userSettings = UserSettings.shared
   @ObservedObject var scriptManager: ScriptManager
   
-  @Query private var sidebarItems: [SidebarItem]
-  @Query private var sidebarFolders: [SidebarFolder]
-
   // RequiredInputPaths
   @State private var showingRequiredInputPathsView = false
   @State private var hasDismissedRequiredInputPathsView = false
@@ -61,14 +58,29 @@ struct ContentView: View {
   
   @State private var columnVisibility = NavigationSplitViewVisibility.all//.doubleColumn
   
+  
+  @Query private var sidebarItems: [SidebarItem]
+  @Query private var sidebarFolders: [SidebarFolder]
   @State private var selectedItemID: UUID?
+  @State private var editingItemId: UUID? = nil
+  @State private var draftTitle: String = ""
+  
+  func saveEditedTitle(_ id: UUID, _ title: String) {
+    if let index = sidebarItems.firstIndex(where: { $0.id == id }) {
+      // Update the title of the found SidebarItem
+      sidebarItems[index].title = title
+      
+      // Save the updated context
+      saveData()
+    }
+  }
   
   func getCurrentPromptToArchive() -> (PromptModel, [URL]) {
     return (currentPrompt, lastSavedImageUrls)
   }
   
   func saveCurrentPromptToData(title: String) {
-    savePromptToData(title: "Some Prompt", prompt: currentPrompt, imageUrls: lastSavedImageUrls)
+    savePromptToData(title: title, prompt: currentPrompt, imageUrls: lastSavedImageUrls)
   }
   
   func savePromptToData(title: String, prompt: PromptModel, imageUrls: [URL]) {
@@ -83,6 +95,46 @@ struct ContentView: View {
     let newFolder = SidebarFolder(name: title)
     modelContext.insert(newFolder)
     saveData()
+  }
+  
+  private func deleteItem(withId id: UUID) {
+    guard let index = sidebarItems.firstIndex(where: { $0.id == id }) else { return }
+    
+    // Perform the deletion
+    let itemToDelete = sidebarItems[index]
+    modelContext.delete(itemToDelete)
+    do {
+      try modelContext.save()
+    } catch {
+      Debug.log("Failed to delete item: \(error.localizedDescription)")
+    }
+    
+    // Determine the next selection
+    let nextSelectionIndex = determineNextSelectionIndex(afterDeleting: index)
+    
+    // Update the selection
+    updateSelection(to: nextSelectionIndex)
+  }
+  
+  private func determineNextSelectionIndex(afterDeleting index: Int) -> Int? {
+    if index > 0 {
+      // Select the item above if available
+      return index - 1
+    } else if sidebarItems.count > 1 {
+      // Select the next item in the list if the deleted item was the first
+      return 0
+    } else {
+      // No items left to select
+      return nil
+    }
+  }
+  
+  private func updateSelection(to index: Int?) {
+    if let newIndex = index, sidebarItems.indices.contains(newIndex) {
+      selectedItemID = sidebarItems[newIndex].id
+    } else {
+      selectedItemID = nil
+    }
   }
   
   func saveData() {
@@ -101,18 +153,35 @@ struct ContentView: View {
             Text(folder.name)
           }
         }
-        Section(header: Text("Items")) {
+        Section(header: Text("Uncategorized")) {
           ForEach(sidebarItems) { item in
-            Text(item.title)
-              .tag(item.id) // Ensure your model has an 'id' property
+            if editingItemId == item.id {
+              TextField("Title", text: $draftTitle, onCommit: {
+                saveEditedTitle(item.id, draftTitle)
+                editingItemId = nil
+              })
+              .textFieldStyle(RoundedBorderTextFieldStyle())
+              .onAppear {
+                draftTitle = item.title
+              }
+            } else {
+              Text(item.title)
+                .tag(item.id)
+                .gesture(TapGesture(count: 1).onEnded {
+                  self.selectedItemID = item.id
+                }.simultaneously(with: TapGesture(count: 2).onEnded {
+                  editingItemId = item.id
+                  draftTitle = item.title
+                }))
+            }
           }
         }
       }
       .listStyle(SidebarListStyle())
-      .onChange(of: selectedItemID) { newItemID, _ in
+      .onChange(of: selectedItemID) { currentItem, newItemID in
+        Debug.log("Selected item ID changed to: \(String(describing: newItemID))")
         if let newItemID = newItemID,
            let selectedItem = sidebarItems.first(where: { $0.id == newItemID }) {
-          // Update currentPrompt with the selected item's prompt
           let modelDataMapping = ModelDataMapping()
           
           if let appPromptModel = selectedItem.prompt {
@@ -125,6 +194,17 @@ struct ContentView: View {
           }
         }
       }
+      .onAppear {
+        NSEvent.addLocalMonitorForEvents(matching: .keyDown) { event in
+          if event.keyCode == 51 { // 51 is the delete key code
+            if let selectedItemID = selectedItemID {
+              deleteItem(withId: selectedItemID)
+            }
+          }
+          return event
+        }
+      }
+      Spacer()
       HStack {
         Button(action: {
           newFolderToData(title: "Some Folder")
