41b02de60c541892cffe6bb96735dd03887ea2ff
Justin Bush <jdevbush@gmail.com>
2024-02-13 20:48:38 -0700

    refactor: SidebarModel, imageUrls

--- SwiftDiffusion/PromptView/Sidebar/SidebarModel.swift
+++ SwiftDiffusion/PromptView/Sidebar/SidebarModel.swift
@@ -12,13 +12,15 @@ import SwiftData
 class SidebarItem {
   @Attribute var title: String
   @Attribute var timestamp: Date
+  @Attribute var imageUrls: [URL]
   @Relationship var prompt: AppPromptModel?
   
-  init(title: String, timestamp: Date = Date(), prompt: AppPromptModel? = nil) {
-      self.title = title
-      self.timestamp = timestamp
-      self.prompt = prompt
-    }
+  init(title: String, timestamp: Date = Date(), imageUrls: [URL], prompt: AppPromptModel? = nil) {
+    self.title = title
+    self.timestamp = timestamp
+    self.imageUrls = imageUrls
+    self.prompt = prompt
+  }
 }
 
 @Model
@@ -36,35 +38,35 @@ class AppPromptModel {
   @Relationship var selectedModel: AppModelItem?
   
   init(positivePrompt: String = "", negativePrompt: String = "", width: Double = 512, height: Double = 512, cfgScale: Double = 7, samplingSteps: Double = 20, seed: String = "-1", batchCount: Double = 1, batchSize: Double = 1, clipSkip: Double = 1, selectedModel: AppModelItem? = nil) {
-      self.positivePrompt = positivePrompt
-      self.negativePrompt = negativePrompt
-      self.width = width
-      self.height = height
-      self.cfgScale = cfgScale
-      self.samplingSteps = samplingSteps
-      self.seed = seed
-      self.batchCount = batchCount
-      self.batchSize = batchSize
-      self.clipSkip = clipSkip
-      self.selectedModel = selectedModel
-    }
+    self.positivePrompt = positivePrompt
+    self.negativePrompt = negativePrompt
+    self.width = width
+    self.height = height
+    self.cfgScale = cfgScale
+    self.samplingSteps = samplingSteps
+    self.seed = seed
+    self.batchCount = batchCount
+    self.batchSize = batchSize
+    self.clipSkip = clipSkip
+    self.selectedModel = selectedModel
+  }
 }
 
 @Model
 class AppModelItem {
   @Attribute var name: String
-  @Attribute var type: AppModelType // Make sure AppModelType is Codable
+  @Attribute var type: AppModelType
   @Attribute var url: URL
   @Attribute var isDefaultModel: Bool = false
   @Relationship var sdModel: AppSdModel?
   
   init(name: String, type: AppModelType, url: URL, isDefaultModel: Bool = false, sdModel: AppSdModel? = nil) {
-      self.name = name
-      self.type = type
-      self.url = url
-      self.isDefaultModel = isDefaultModel
-      self.sdModel = sdModel
-    }
+    self.name = name
+    self.type = type
+    self.url = url
+    self.isDefaultModel = isDefaultModel
+    self.sdModel = sdModel
+  }
 }
 
 func mapModelTypeToAppModelType(_ type: ModelType?) -> AppModelType? {
@@ -92,13 +94,13 @@ class AppSdModel {
   @Attribute var config: String?
   
   init(title: String, modelName: String, hash: String? = nil, sha256: String? = nil, filename: String, config: String? = nil) {
-      self.title = title
-      self.modelName = modelName
-      self.hash = hash
-      self.sha256 = sha256
-      self.filename = filename
-      self.config = config
-    }
+    self.title = title
+    self.modelName = modelName
+    self.hash = hash
+    self.sha256 = sha256
+    self.filename = filename
+    self.config = config
+  }
 }
 
 @Model
@@ -113,14 +115,14 @@ class SidebarFolder {
 }
 
 /*
-struct SidebarView: View {
-  @Query var folders: [SidebarFolder] // Use the Query property wrapper to fetch folders
-  
-  var body: some View {
-    List(folders, children: \.contents) { item in
-      // Representation of SidebarItem or SidebarFolder
-      Text(item.name) // Adjust based on your item or folder properties
-    }
-  }
-}
-*/
+ struct SidebarView: View {
+ @Query var folders: [SidebarFolder] // Use the Query property wrapper to fetch folders
+ 
+ var body: some View {
+ List(folders, children: \.contents) { item in
+ // Representation of SidebarItem or SidebarFolder
+ Text(item.name) // Adjust based on your item or folder properties
+ }
+ }
+ }
+ */
