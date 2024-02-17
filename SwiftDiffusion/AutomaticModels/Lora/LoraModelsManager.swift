ad60f3a620ea69216f06f6613cdd1a4a67c7e222
Justin Bush <jdevbush@gmail.com>
2024-02-17 10:58:32 -0700

    feat: LoraService

--- SwiftDiffusion/AutomaticModels/Lora/LoraModelsManager.swift
+++ SwiftDiffusion/AutomaticModels/Lora/LoraModelsManager.swift
@@ -7,11 +7,6 @@
 
 import Foundation
 
-extension Constants.API.Endpoint {
-  static let getLoras = "/sdapi/v1/loras"
-  static let postRefreshLoras = "/sdapi/v1/refresh-loras"
-}
-
 class LoraModelsManager: ObservableObject {
   @Published var loraModels: [LoraModel] = []
   private var directoryObserver: DirectoryObserver?
@@ -48,17 +43,5 @@ class LoraModelsManager: ObservableObject {
         }
       }
     }
-  }
-}
-
-
-class LoraService {
-  static func fetchLoras() async throws -> [LoraModel] {
-    let data = try await AutomaticApiService.shared.request(endpoint: Constants.API.Endpoint.getLoras)
-    return try JSONDecoder().decode([LoraModel].self, from: data)
-  }
-  
-  static func refreshLoras() async throws {
-      try _ = await AutomaticApiService.shared.request(endpoint: Constants.API.Endpoint.postRefreshLoras, httpMethod: "POST")
   }
 }
