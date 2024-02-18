b830444b91129d765fa1db078d4d9a7f7654adbd
Justin Bush <jdevbush@gmail.com>
2024-02-17 18:48:45 -0700

    refactor: PromptModel console output

--- SwiftDiffusion/PromptView/PromptModel.swift
+++ SwiftDiffusion/PromptView/PromptModel.swift
@@ -34,7 +34,7 @@ class PromptModel: ObservableObject {
 extension PromptModel {
   func updateProperties(from model: PromptModel) {
     Debug.log("updateProperties from\n        selectedModel: \(String(describing: selectedModel?.name))")
-    Debug.log("                    checkpointMetadata.title: \(String(describing: selectedModel?.checkpointMetadata?.title))")
+    Debug.log("        checkpointMetadata.title: \(String(describing: selectedModel?.checkpointMetadata?.title))")
     
     self.isWorkspaceItem = model.isWorkspaceItem
     
