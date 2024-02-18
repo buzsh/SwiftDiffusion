74eeada27fbf9fb2270ff5402a253c7979cf0a8b
Justin Bush <jdevbush@gmail.com>
2024-02-17 18:59:32 -0700

    refactor: user-db.store

--- SwiftDiffusion/SwiftDiffusionApp.swift
+++ SwiftDiffusion/SwiftDiffusionApp.swift
@@ -29,7 +29,7 @@ struct SwiftDiffusionApp: App {
     let storeURL = appSupportURL
       .appendingPathComponent(Constants.FileStructure.AppSupportFolderName)
       .appendingPathComponent("UserData").appendingPathComponent("LocalDatabase")
-      .appendingPathComponent(Constants.FileStructure.AppSwiftDataFileName)
+      .appendingPathComponent("user-db.store") // Constants.FileStructure.AppSwiftDataFileName
     
     let subfolderURL = storeURL.deletingLastPathComponent()
     if !fileManager.fileExists(atPath: subfolderURL.path) {
