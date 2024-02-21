dc812c37e223e72c379190af826c400779d7f44f
Justin Bush <jdevbush@gmail.com>
2024-02-21 07:08:32 -0700

    refactor: organize, separate entries

---
+++ SwiftDiffusion/AutomaticModels/Checkpoints/Models/CheckpointApiModel.swift
@@ -0,0 +1,33 @@
+//
+//  CheckpointApiModel.swift
+//  SwiftDiffusion
+//
+//  Created by Justin Bush on 2/21/24.
+//
+
+import Foundation
+
+struct CheckpointApiModel: Decodable {
+  let title: String
+  let modelName: String
+  let hash: String?
+  let sha256: String?
+  let filename: String
+  let config: String?
+  
+  enum CodingKeys: String, CodingKey {
+    case title
+    case modelName = "model_name"
+    case hash
+    case sha256
+    case filename
+    case config
+  }
+}
+
+struct ClientConfig: Encodable, Decodable {
+  let sdModelCheckpoint: String
+  enum CodingKeys: String, CodingKey {
+    case sdModelCheckpoint = "sd_model_checkpoint"
+  }
+}
--- SwiftDiffusion/AutomaticModels/Checkpoints/Models/CheckpointModel.swift
+++ SwiftDiffusion/AutomaticModels/Checkpoints/Models/CheckpointModel.swift
@@ -1,12 +1,11 @@
 //
-//  CheckpointApiModel.swift
+//  CheckpointModel.swift
 //  SwiftDiffusion
 //
 //  Created by Justin Bush on 2/18/24.
 //
 
 import Foundation
-import Combine
 
 enum CheckpointModelType {
   case coreMl
@@ -31,30 +30,5 @@ class CheckpointModel: ObservableObject, Identifiable {
 extension CheckpointModel: Equatable {
   static func == (lhs: CheckpointModel, rhs: CheckpointModel) -> Bool {
     return lhs.id == rhs.id
-  }
-}
-
-struct CheckpointApiModel: Decodable {
-  let title: String
-  let modelName: String
-  let hash: String?
-  let sha256: String?
-  let filename: String
-  let config: String?
-  
-  enum CodingKeys: String, CodingKey {
-    case title
-    case modelName = "model_name"
-    case hash
-    case sha256
-    case filename
-    case config
-  }
-}
-
-struct ClientConfig: Encodable, Decodable {
-  let sdModelCheckpoint: String
-  enum CodingKeys: String, CodingKey {
-    case sdModelCheckpoint = "sd_model_checkpoint"
   }
 }
