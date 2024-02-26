c07480e157aeef165b48632e6d6656ac700d611c
Justin Bush <jdevbush@gmail.com>
2024-02-26 13:13:31 -0700

    feat: createImagePreviews

--- SwiftDiffusion/Views/MainViews/SidebarView/SidebarViewModel+Thumbnails.swift
+++ SwiftDiffusion/Views/MainViews/SidebarView/SidebarViewModel+Thumbnails.swift
@@ -11,6 +11,55 @@ import AppKit
 import SwiftData
 
 extension SidebarViewModel {
+  func createImagePreviews(for sidebarItem: SidebarItem, in model: ModelContext, maxDimension: CGFloat = 1000, compressionFactor: CGFloat = 0.5) {
+    let fileManager = FileManager.default
+    guard let outputDirectoryUrl = UserSettings.shared.outputDirectoryUrl,
+          let basePreviewURL = try? fileManager.url(for: .applicationSupportDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
+            .appendingPathComponent(Constants.FileStructure.AppSupportFolderName)
+            .appendingPathComponent("UserData")
+            .appendingPathComponent("LocalDatabase")
+            .appendingPathComponent("StoredPromptPreviews") else {
+      Debug.log("[createImagePreviews] Unable to find or create the application support directory.")
+      return
+    }
+    
+    var previewUrls: [URL] = []
+    
+    sidebarItem.imageUrls.forEach { imageUrl in
+      guard let image = NSImage(contentsOf: imageUrl) else {
+        Debug.log("[createImagePreviews] Failed to load image at \(imageUrl)")
+        return
+      }
+      
+      // Resize the image and compress it to JPEG with the specified parameters
+      guard let imageData = image.resizedAndCompressedImageData(maxDimension: maxDimension, compressionFactor: compressionFactor) else {
+        Debug.log("[createImagePreviews] Failed to process image at \(imageUrl)")
+        return
+      }
+      
+      let relativePath = imageUrl.path.replacingOccurrences(of: outputDirectoryUrl.path, with: "")
+      let newImageUrl = basePreviewURL.appendingPathComponent(relativePath)
+      
+      do {
+        let directoryUrl = newImageUrl.deletingLastPathComponent()
+        try fileManager.createDirectory(at: directoryUrl, withIntermediateDirectories: true, attributes: nil)
+        
+        try imageData.write(to: newImageUrl)
+        previewUrls.append(newImageUrl)
+      } catch {
+        Debug.log("[createImagePreviews] Failed to save preview for \(imageUrl): \(error)")
+      }
+    }
+    
+    DispatchQueue.main.async {
+      sidebarItem.imagePreviewUrls = previewUrls
+      // Implement any necessary model update logic here, such as saving changes to the model context
+      self.saveData(in: model)
+    }
+  }
+}
+
+extension SidebarViewModel {
   func createImageThumbnails(for sidebarItem: SidebarItem, in model: ModelContext, maxDimension: CGFloat = 250, compressionFactor: CGFloat = 0.5) {
     let fileManager = FileManager.default
     guard let outputDirectoryUrl = UserSettings.shared.outputDirectoryUrl,
