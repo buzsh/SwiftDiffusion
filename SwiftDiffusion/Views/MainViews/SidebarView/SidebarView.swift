04f4807e2b9e1ab8116e6a8a7dc5d5cc5d03ada0
Justin Bush <jdevbush@gmail.com>
2024-02-26 19:38:48 -0700

    refactor: rollback preload main images for now

--- SwiftDiffusion/Views/MainViews/SidebarView/SidebarView.swift
+++ SwiftDiffusion/Views/MainViews/SidebarView/SidebarView.swift
@@ -382,29 +382,31 @@ struct SidebarView: View {
   }
   
   private func preloadImages(for items: [SidebarItem]) {
-      items.forEach { item in
-          // Preload main images
-          item.imageUrls.forEach { imageUrl in
-              preloadImage(from: imageUrl)
-          }
-          // Preload thumbnails
-          item.imageThumbnails.forEach { imageInfo in
-              preloadImage(from: imageInfo.url)
-          }
-          // Preload previews
-          item.imagePreviews.forEach { imageInfo in
-              preloadImage(from: imageInfo.url)
-          }
+    items.forEach { item in
+      // Preload main images
+      /*
+      item.imageUrls.forEach { imageUrl in
+        preloadImage(from: imageUrl)
       }
+       */
+      // Preload thumbnails
+      item.imageThumbnails.forEach { imageInfo in
+        preloadImage(from: imageInfo.url)
+      }
+      // Preload previews
+      item.imagePreviews.forEach { imageInfo in
+        preloadImage(from: imageInfo.url)
+      }
+    }
   }
-
+  
   private func preloadImage(from url: URL) {
-      DispatchQueue.global(qos: .background).async {
-          guard ImageCache.shared.image(forKey: url.path) == nil, let image = NSImage(contentsOf: url) else { return }
-          DispatchQueue.main.async {
-              ImageCache.shared.setImage(image, forKey: url.path)
-          }
+    DispatchQueue.global(qos: .background).async {
+      guard ImageCache.shared.image(forKey: url.path) == nil, let image = NSImage(contentsOf: url) else { return }
+      DispatchQueue.main.async {
+        ImageCache.shared.setImage(image, forKey: url.path)
       }
+    }
   }
   
 }
