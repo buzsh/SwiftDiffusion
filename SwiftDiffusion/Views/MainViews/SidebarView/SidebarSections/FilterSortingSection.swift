bdf6319857d7832a9917054c1c1b1074a2e40696
Justin Bush <jdevbush@gmail.com>
2024-02-28 08:47:22 -0700

    refactor: FilterSortingSection toggle visible

--- SwiftDiffusion/Views/MainViews/SidebarView/SidebarSections/FilterSortingSection.swift
+++ SwiftDiffusion/Views/MainViews/SidebarView/SidebarSections/FilterSortingSection.swift
@@ -10,27 +10,32 @@ import SwiftUI
 struct FilterSortingSection: View {
   @Binding var sortingOrder: SidebarView.SortingOrder
   @Binding var selectedModelName: String?
+  @Binding var filterToolsButtonToggled: Bool
   let uniqueModelNames: [String]
   
   var body: some View {
-    List {
-      Section(header: Text("Sorting")) {
-        Menu(sortingOrder.rawValue) {
-          Button("Most Recent") { sortingOrder = .mostRecent }
-          Button("Least Recent") { sortingOrder = .leastRecent }
-        }
-      }
-      
-      Section(header: Text("Filters")) {
-        Menu(selectedModelName ?? "Filter by Model") {
-          Button("Show All") { selectedModelName = nil }
-          Divider()
-          ForEach(uniqueModelNames, id: \.self) { modelName in
-            Button(modelName) { selectedModelName = modelName }
+    if filterToolsButtonToggled {
+      VStack {
+        List {
+          Section(header: Text("Sorting")) {
+            Menu(sortingOrder.rawValue) {
+              Button("Most Recent") { sortingOrder = .mostRecent }
+              Button("Least Recent") { sortingOrder = .leastRecent }
+            }
+          }
+          
+          Section(header: Text("Filters")) {
+            Menu(selectedModelName ?? "Filter by Model") {
+              Button("Show All") { selectedModelName = nil }
+              Divider()
+              ForEach(uniqueModelNames, id: \.self) { modelName in
+                Button(modelName) { selectedModelName = modelName }
+              }
+            }
           }
         }
       }
+      .frame(height: 110)
     }
-    .frame(height: 110)
   }
 }
